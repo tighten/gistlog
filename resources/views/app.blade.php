0dc83dcee13f4e80ab30eab5736dde2db73d74f0
Matt Stauffer <matt@tighten.co>
2015-01-30 14:56:05 -0500

    Add analytics

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -47,5 +47,18 @@
 	<!-- Scripts -->
 	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 	<script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
+
+	@if (env('APP_ENV') == 'production') 
+	<script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+		ga('create', 'UA-40114814-7', 'auto');
+		ga('send', 'pageview');
+
+	</script>
+	@endif
 </body>
 </html>
