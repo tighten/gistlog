12c90bddf1fff1a51a13bb1f16ce7051d32cfadb
Matt Stauffer <matt@tighten.co>
2015-02-01 17:00:14 -0500

    Add Rick Astley 404 error.

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,42 @@
+<html>
+<head>
+    <link href='http://fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
+
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+            width: 100%;
+            height: 100%;
+            color: #B0BEC5;
+            display: table;
+            font-weight: 100;
+            font-family: 'Lato';
+        }
+
+        .container {
+            text-align: center;
+            display: table-cell;
+            vertical-align: middle;
+        }
+
+        .content {
+            text-align: center;
+            display: inline-block;
+        }
+
+        .title {
+            font-size: 72px;
+            margin-bottom: 40px;
+        }
+    </style>
+</head>
+<body>
+<div class="container">
+    <div class="content">
+        <div class="title">404! Watch this instead!</div>
+        <iframe width="420" height="315" src="https://www.youtube-nocookie.com/embed/dQw4w9WgXcQ?rel=0" frameborder="0" allowfullscreen></iframe>
+    </div>
+</div>
+</body>
+</html>
