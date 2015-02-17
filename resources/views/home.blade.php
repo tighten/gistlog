8752535375aed6ea49bbba6616d0f1412f013459
Matt Stauffer <matt@tighten.co>
2015-02-17 11:03:15 -0500

    Add some custom mobile styles; link to default description post; Fix broken mobile nav hider.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,13 +6,28 @@
             <div class="col-md-10 col-md-offset-1">
                 <h1>Create a Gistlog</h1>
 
-                <form method="post" action="/posts/create" class="form-inline">
-                    <div class="form-group">
-                        <label for="gistUrl">Paste the URL to a Gist here to create a Gistlog:</label><br>
-                        <input class="form-control" id="gistUrl" name="gistUrl" placeholder="Enter Gist URL">
-                        <button type="submit" class="btn btn-default">Submit</button>
-                    </div>
-                </form>
+                <div class="bg-primary" style="padding: 1em;">
+                    <form method="post" action="/posts/create">
+                        <div class="form-group">
+                            <label for="gistUrl">Paste the URL to a Gist here to create a Gistlog:</label><br>
+                            <div class="row">
+                                <div class="col-md-7">
+                                    <input class="form-control" id="gistUrl" name="gistUrl" placeholder="Enter Gist URL">
+                                </div>
+                                <div class="col-md-4">
+                                    <button type="submit" class="btn btn-default">Create</button>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+
+                <br><br>
+
+                <div class="bg-info" style="padding: 1em;">
+                    <p><strong>Tip:</strong> Try this gist (just copy the URL, paste it above, and hit Create) to see how Gistlog works and also to learn a little bit more about what Gistlog is:</p>
+                    <input class="form-control" type="text" value="https://gist.github.com/mattstauffer/1c76d40371b295184845">
+                </div>
             </div>
         </div>
     </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,7 +26,7 @@
 	<nav class="navbar navbar-default">
 		<div class="container-fluid">
 			<div class="navbar-header">
-				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#primary-nav-collapse">
 					<span class="u-sr-only">Toggle Navigation</span>
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
@@ -35,7 +35,7 @@
 				<a class="navbar-brand" href="#">Gistlog</a>
 			</div>
 
-			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+			<div class="collapse navbar-collapse" id="primary-nav-collapse">
 				<ul class="nav navbar-nav">
 					<li><a href="/">Create a New Gistlog</a></li>
 				</ul>
