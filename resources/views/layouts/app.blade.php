12d86cfc7718df5ccbdecb78d544391484d850df
Matt Stauffer <matt@tighten.co>
2015-02-17 11:27:30 -0500

    Point home button to home because @ianlandsman is handsome

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,7 +32,7 @@
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 				</button>
-				<a class="navbar-brand" href="#">Gistlog</a>
+				<a class="navbar-brand" href="/">Gistlog</a>
 			</div>
 
 			<div class="collapse navbar-collapse" id="primary-nav-collapse">
