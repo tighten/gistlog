f5126254c51257bd13cf610cf7d18d0caccf08d5
Jacob Bennett <me@jakebennett.net>
2015-07-23 01:41:38 -0500

    Update styles and structure of author listing page

--- resources/views/authors/show.blade.php
+++ resources/views/authors/show.blade.php
@@ -2,19 +2,28 @@
 
 @section('content')
     <div class="container">
-        <a href="https://github.com/{{ $author->username }}" class="profile-pic">
-            <img src="{{ $author->avatarUrl }}">
-        </a>
-
-        <article class="gistlog">
+        <section class="gistlog-author-block">
+            <a href="https://github.com/{{ $author->username }}" class="profile-pic">
+                <img src="{{ $author->avatarUrl }}">
+            </a>
             <h1 class="gistlog__title">{{ $author->name }}</h1>
             <span class="gistlog__author">({{ "@" . $author->username }})</span>
+        </section>
 
-            <ul>
+        <section>
+            <ul class="list-unstyled">
                 @foreach ($author->gists as $gist)
-                    <li>{{ $gist->createdAt->format('Y-m-d') }} - <a href="/{{ $author->username }}/{{ $gist->id }}">{{ $gist->title }}</a></li>
+                    <li class="gistlog-article">
+                         <a href="/{{ $author->username }}/{{ $gist->id }}" class="gistlog-article__title">{{ $gist->title }}</a>
+                        <span class="gistlog-article__timestamp">
+                            Posted {{ $gist->createdAt->diffForHumans() }}
+                        </span>
+                        <p class="gistlog-article__summary">
+                            {{ $gist->getPreview() }}
+                        </p>
+                    </li>
                 @endforeach
             </ul>
-        </article>
+        </section>
     </div>
 @endsection
