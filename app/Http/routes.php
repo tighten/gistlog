f16c308f87b30e36f53cebb16c93503b83b12431
Jacob Bennett <me@jakebennett.net>
2015-08-17 08:12:33 -0500

    feat: Create routes and modify AuthController to accommodate Github login and user creation via Socialite

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,6 +1,12 @@
 <?php
 
+Route::any('test/{gistId}', ['uses' => 'GistsController@postComment']);
+
 Route::get('/', ['uses' => 'HomeController@index', 'as' => 'home']);
+
+Route::get('logout', 'Auth\AuthController@getLogout');
+Route::get('auth/github', 'Auth\AuthController@redirectToProvider');
+Route::get('auth/github/callback', 'Auth\AuthController@handleProviderCallback');
 
 Route::post('posts/create', 'GistsController@storeAndRedirect');
 
