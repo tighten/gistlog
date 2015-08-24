6ab12e457195a295af31905d5bc263e99cdcc389
Jacob Bennett <me@jakebennett.net>
2015-08-23 20:10:15 -0500

    fix: tab and space cleanup

--- config/app.php
+++ config/app.php
@@ -135,7 +135,7 @@ return [
 		'Illuminate\Translation\TranslationServiceProvider',
 		'Illuminate\Validation\ValidationServiceProvider',
 		'Illuminate\View\ViewServiceProvider',
-        'Illuminate\Broadcasting\BroadcastServiceProvider',
+		'Illuminate\Broadcasting\BroadcastServiceProvider',
 
 		/*
 		 * Application Service Providers...
