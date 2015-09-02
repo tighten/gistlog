90c3df0ce3d421dc0bb9515ef398824c488af7bc
Adam Wathan <adam.wathan@gmail.com>
2015-09-02 09:41:33 -0400

    Allow pre tags and class attributes on code tags

--- app/ContentParser/XssCleaner.php
+++ app/ContentParser/XssCleaner.php
@@ -1,11 +1,17 @@
 <?php namespace Gistlog\ContentParser;
 
+use Stauros\HTML\Config;
 use Stauros\Stauros;
 
 class XssCleaner implements Transformer
 {
     public function transform($content)
     {
-        return (new Stauros)->scanHTML($content);
+        $config = new Config;
+        $config->tagWhiteList = array_merge($config->tagWhiteList, [
+            'code' => ['class' => true],
+            'pre' => [],
+        ]);
+        return (new Stauros($config))->scanHTML($content);
     }
 }
