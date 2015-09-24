9b9cf57dc2492ec0c1839a8e3febbfb3b518c2a0
Jeff Finley <quickliketurtle@gmail.com>
2015-09-24 00:24:23 -0700

    updated markdown transformer to use github api

--- app/ContentParser/MarkdownTransformer.php
+++ app/ContentParser/MarkdownTransformer.php
@@ -1,11 +1,19 @@
 <?php namespace Gistlog\ContentParser;
 
-use Michelf\MarkdownExtra;
+use Github\Client;
+use Github\HttpClient\Message\ResponseMediator;
 
 class MarkdownTransformer implements Transformer
 {
+    private $client;
+
+    public function __construct()
+    {
+        $this->client = new Client;
+    }
+
     public function transform($content)
     {
-        return MarkdownExtra::defaultTransform($content);
+        return $this->client->api('markdown')->render($content);
     }
 }
