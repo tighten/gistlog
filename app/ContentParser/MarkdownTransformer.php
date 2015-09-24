ddce8c3e2ca03d3a63c795630d7193e46caf133a
Jeff Finley <quickliketurtle@gmail.com>
2015-09-24 13:21:03 -0700

    reverted to origional Markdown parser

--- app/ContentParser/MarkdownTransformer.php
+++ app/ContentParser/MarkdownTransformer.php
@@ -1,19 +1,11 @@
 <?php namespace Gistlog\ContentParser;
 
-use Github\Client;
-use Github\HttpClient\Message\ResponseMediator;
+use Michelf\MarkdownExtra;
 
 class MarkdownTransformer implements Transformer
 {
-    private $client;
-
-    public function __construct()
-    {
-        $this->client = new Client;
-    }
-
     public function transform($content)
     {
-        return $this->client->api('markdown')->render($content);
+        return MarkdownExtra::defaultTransform($content);
     }
 }
