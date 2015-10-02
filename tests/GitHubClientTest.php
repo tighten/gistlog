1c2e7bff0bb226daefa38e39c382e8d18ef67c6a
Jeff Finley <quickliketurtle@gmail.com>
2015-10-01 19:00:51 -0700

    re-factor GitHubClientTest to not use rate_limit method since it does not exist in the php-github-api package yet.

--- tests/GitHubClientTest.php
+++ tests/GitHubClientTest.php
@@ -4,13 +4,14 @@ use Github\Client as GitHubClient;
 
 class GitHubClientTest extends TestCase
 {
-
     /** @test */
-    public function it_authenticates_with_github_and_returns_5000_rate_limit()
+    public function it_authenticates_with_github_and_returns_5000_rate_limit_guzzle()
     {
         $github = App::make(GitHubClient::class);
 
-        $limit = $github->api('rate_limit')->getCoreLimit();
+        $response = $github->getHttpClient()->get('rate_limit')->json();
+
+        $limit = $response['resources']['core']['limit'];
 
         $this->assertEquals('5000', $limit);
     }
