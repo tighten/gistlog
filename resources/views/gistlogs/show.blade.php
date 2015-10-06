85a3e6a540e3c9a0099aa1ed1395d2281ea9b858
Matt Stauffer <matt@tighten.co>
2015-10-06 19:13:13 -0400

    triple dork

--- resources/views/gistlogs/show.blade.php
+++ resources/views/gistlogs/show.blade.php
@@ -35,6 +35,21 @@
 
 @section('scripts')
     <script src="/js/commentForm.js"></script>
+    <script>
+    $(function() {
+        commentForm.init();
+
+        // $('.js-gistlog-content pre').each(function () {
+        //     var numberOfLines = $(this).find('code').html().split(/\n/).length - 1;
+        //     var lineNumbers = [];
+        //     for (var i = 1; i <= numberOfLines; i++) {
+        //         lineNumbers.push(i);
+        //     }
+        //     $(this).append('<div class="line-numbers">' + lineNumbers.join("\n") + '</div>');
+        // });
+        autosize($('textarea'));
+    });
+    </script>
 @endsection
 
 @section('meta')
