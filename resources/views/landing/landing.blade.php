425dbe518f445af7c9ecc13b07ac1177c11b4485
Sebastian Walker <info@walkermedia.de>
2016-10-06 16:04:36 +0200

    Changed landing font to Roboto.

--- resources/views/landing/landing.blade.php
+++ resources/views/landing/landing.blade.php
@@ -3,7 +3,7 @@
     <head>
         <title>Gistlog - Your dev blog delivered</title>
         <link rel="stylesheet" href="{{asset('css/landing.css')}}">
-        <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+        <link rel="stylesheet" href="//brick.a.ssl.fastly.net/Roboto:300,400,700">
         <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-T8Gy5hrqNKT+hzMclPo118YTQO6cYprQmhrYwIiQ/3axmI1hQomh7Ud2hPOy8SP1" crossorigin="anonymous">
     </head>
     <body>
