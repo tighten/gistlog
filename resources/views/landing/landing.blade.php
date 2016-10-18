43dafcbfe8c37094d090ab697574da486e1d438a
Matt Stauffer <matt@tighten.co>
2016-10-18 14:10:19 -0400

    og image

--- resources/views/landing/landing.blade.php
+++ resources/views/landing/landing.blade.php
@@ -25,6 +25,7 @@
         <!-- Open Graph data -->
         <meta property="og:title" content="GistLog - Your dev blog delivered">
         <meta property="og:type" content="product">
+        <meta property="og:image" content="{{ asset('img/gistlog-og.png') }}">
         <meta property="og:url" content="{{ Request::url() }}">
         <meta property="og:description" content="Your dev blog - delivered! GistLog allows you to publish your blog posts via simple GitHub gists.">
         <meta property="og:site_name" content="GistLog">
