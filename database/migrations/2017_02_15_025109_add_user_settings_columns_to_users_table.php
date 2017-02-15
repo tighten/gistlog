60b3fea75b3e0e8e5f693b4a2da5373ac94c3af7
Jacob Bennett <me@jakebennett.net>
2017-02-14 21:39:07 -0600

    feat: add additional settings columns to user table

---
+++ database/migrations/2017_02_15_025109_add_user_settings_columns_to_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddUserSettingsColumnsToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('google_analaytics_id')->nullable()->after('token');
+            $table->string('twitter_username')->nullable()->after('token');
+            $table->string('homepage')->nullable()->after('token');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn(['twitter_username', 'homepage', 'google_analaytics_id']);
+        });
+    }
+}
