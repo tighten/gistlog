33d3b8f7f8aaefb5dcab73481cc91ade1c26a1f9
Jacob Bennett <me@jakebennett.net>
2017-02-22 08:47:00 -0600

    feat: added form errors to flash-messages partial.

--- resources/views/partials/flash-messages.blade.php
+++ resources/views/partials/flash-messages.blade.php
@@ -1,4 +1,4 @@
-@if (Session::has('success-message') || Session::has('error-message') || Session::has('message'))
+@if (Session::has('success-message') || Session::has('error-message') || Session::has('message') || count($errors) > 0)
 <div class="container">
     @if (Session::has('success-message'))
     <div class="notice notice--success" data-dismiss="timeout">
@@ -13,6 +13,15 @@
     @if (Session::has('message'))
     <div class="notice notice--info" data-dismiss="timeout">
         {{ Session::get('message') }}
+    </div>
+    @endif
+    @if (count($errors) > 0)
+    <div class="notice notice--error">
+        <ul style="margin: 0; padding: 0;">
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
     </div>
     @endif
 </div>
