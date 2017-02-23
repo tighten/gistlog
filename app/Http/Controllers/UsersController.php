d01d8ffd472c23dd5737dad4e03d415edd51bb40
Jacob Bennett <me@jakebennett.net>
2017-02-22 22:56:17 -0600

    code style changes and improvements

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -6,23 +6,20 @@ use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
-
     public function edit()
     {
-        $user = auth()->user();
-
-        return view('authors.edit', compact('user'));
+        return view('authors.edit')->with('user', auth()->user());
     }
 
     public function update(Request $request)
     {
         $this->validate($request, [
-            'homepage'             => 'max:100|url',
-            'twitter_username'     => 'max:15',
+            'homepage' => 'max:100|url',
+            'twitter_username' => 'max:15',
             'google_analytics_id' => 'max:20',
         ]);
 
-        auth()->user()->update(request()->only(['homepage', 'twitter_username', 'google_analytics_id']));
+        auth()->user()->update($request->only(['homepage', 'twitter_username', 'google_analytics_id']));
 
         $request->session()->flash('success-message', 'User Settings have been updated.');
 
