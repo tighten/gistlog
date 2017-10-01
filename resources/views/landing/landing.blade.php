1ffde75e09a15673980fc1e7723e046126eea608
Christoph Rumpel <c.rumpel@kabsi.at>
2017-10-01 20:23:39 +0200

    Add a favicon to the landing page

--- resources/views/landing/landing.blade.php
+++ resources/views/landing/landing.blade.php
@@ -11,6 +11,7 @@
         <link rel="stylesheet" href="{{ asset('css/landing.css' )}}">
         <link rel="stylesheet" href="//brick.a.ssl.fastly.net/Roboto:300,400,700">
         <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-T8Gy5hrqNKT+hzMclPo118YTQO6cYprQmhrYwIiQ/3axmI1hQomh7Ud2hPOy8SP1" crossorigin="anonymous">
+        <link rel="icon" type="image/png" href="/img/gistlog-fav120.png">
 
         <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
 
