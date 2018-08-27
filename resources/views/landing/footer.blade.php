c61602d03251f290600927df3965675edc35a415
Anthony Terell <anthonygterrell@gmail.com>
2018-08-27 12:51:04 -0400

    Update blue hyperlinks in footer to text-blue-light

--- resources/views/landing/footer.blade.php
+++ resources/views/landing/footer.blade.php
@@ -1,8 +1,8 @@
 <div class="flex bg-grey-darkest min-h-6 justify-center items-center text-white">
     <p>
-        <a href="/" title="GistLog by Tighten" class="text-blue no-underline hover:text-white">GistLog</a>
+        <a href="/" title="GistLog by Tighten" class="text-blue-light no-underline hover:text-white">GistLog</a>
         &copy; {{ Carbon\Carbon::now()->format('Y') }} |
         By the fine folks at
-        <a href="https://tighten.co" title="Tighten" class="text-blue no-underline hover:text-white">Tighten</a>
+        <a href="https://tighten.co" title="Tighten" class="text-blue-light no-underline hover:text-white">Tighten</a>
     </p>
 </div>
