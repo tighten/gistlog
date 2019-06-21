aceef9649ad8f4a69bab622e2ab8d47ee1e1668f
Anthony Terrell <anthonygterrell@gmail.com>
2019-06-21 08:35:48 -0500

    Move Google Analytics UA code to <head>

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    @if (env('APP_ENV') == 'production')
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+            ga('create', 'UA-40114814-7', 'auto');
+            ga('send', 'pageview');
+        </script>
+    @endif
+
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -49,19 +61,6 @@
             <a href="https://tighten.co" class="text-blue-light no-underline hover:text-white ml-1">Tighten</a>
         </div>
     </div>
-
-
-    @if (env('APP_ENV') == 'production')
-        <script>
-            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-                })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-            ga('create', 'UA-40114814-7', 'auto');
-            ga('send', 'pageview');
-        </script>
-    @endif
 
     <script src="/js/app.js"></script>
     @yield('scripts')
