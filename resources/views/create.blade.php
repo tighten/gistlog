798fff3986ab011de84fd1a1ec8bf92b1a39efdc
Anthony Terrell <anthonygterrell@gmail.com>
2019-06-24 10:18:11 -0500

    Adjust typography and colors to pass accessibility

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -5,11 +5,13 @@
     <div class="max-w-md mx-auto mt-20">
         <p class="text-blue-darkest text-base text-center mt-8 mb-3">Paste the URL to a Gist here to create a GistLog</p>
 
-        <form method="POST" action="{{ route('post.create') }}" class="shadow flex flex-1 mb-8">
+        <form method="POST" action="{{ route('post.create') }}" class="flex flex-1 shadow mb-8">
             {{ csrf_field() }}
 
+            <label for="gistUrl" class="hidden"></label>
             <input
                 type="text"
+                id="gistUrl"
                 name="gistUrl"
                 class="bg-white w-4/5 py-3 rounded-tl rounded-bl text-grey text-sm px-4 focus:outline-none" placeholder="https://gist.github.com/username/gist-id">
 
@@ -18,18 +20,19 @@
             >Create</button>
         </form>
 
-        <div class="bg-grey-lighter rounded mt-1 py-4 px-4">
-            <p class="text-sm text-blue-darkest pt-1">Just want to test drive?</p>
-            <p class="text-sm my-4 w-5/6 text-grey-darker text-center w-full leading-normal">
+        <div class="bg-grey-lighter rounded p-4">
+            <p class="text-blue-darkest pt-1">Just want to test drive?</p>
+            <p class="text-sm my-4 w-5/6 text-grey-darkest text-center w-full leading-normal">
                 Try this gist (just copy the URL, paste it above) to see how GistLog works and also to learn a little bit more about what GistLog is:
             </p>
 
+            <label for="example-snippet" class="hidden"></label>
             <input
                 type="text"
+                name="gist-example"
                 id="example-snippet"
                 class="bg-white w-full py-3 text-grey text-sm px-4 rounded border border-grey-light focus:border-grey focus:outline-none" value="https://gist.github.com/mattstauffer/1c76d40371b295184845"
-                onclick="copyToClipboard()"
-            >
+                onclick="copyToClipboard()">
 
             <small id="copy-successful" class="block invisible text-grey-darker text-right my-1">Copied to clipboard</small>
         </div>
