3c23808749928077553cbb91ee5907ee7ef56aa1
Laravel Shift <shift@laravelshift.com>
2019-08-03 19:05:49 +0000

    Shift cleanup

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -58,6 +58,7 @@ class Kernel extends HttpKernel
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
     ];
+
     /**
      * The priority-sorted list of middleware.
      *
