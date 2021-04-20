6bd5a7138475c3e8349fd098e065a11a58394a31
Shahlin Ibrahim <shahlin44@gmail.com>
2021-04-20 21:24:18 +0400

    Added bugsnag to the stack in logging config

--- config/logging.php
+++ config/logging.php
@@ -37,7 +37,7 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['single'],
+            'channels' => ['single', 'bugsnag'],
             'ignore_exceptions' => false,
         ],
 
