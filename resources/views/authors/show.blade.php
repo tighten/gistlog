505d3a37301bcd42891baedeef196e48f84a2643
Matt Stauffer <matt@tighten.co>
2021-12-09 14:11:33 -0500

    Tweak author bio design

--- resources/views/authors/show.blade.php
+++ resources/views/authors/show.blade.php
@@ -1,15 +1,11 @@
 @extends('layouts.app')
 
 @section('meta')
-<link
-    rel="alternate"
-    type="application/atom+xml"
-    title="RSS Feed for {{ $author->username }} on GistLog"
-    href="{{ route('authors.rss.show', $author->username) }}" />
+<link rel="alternate" type="application/atom+xml" title="RSS Feed for {{ $author->username }} on GistLog" href="{{ route('authors.rss.show', $author->username) }}" />
 @endsection
 
 @section('content')
-<div class="gistlog__container container">
+<div class="container gistlog__container">
     <div class="avatar">
         <a href="https://github.com/{{ $author->username }}" target="_blank">
             <img src="{{ $author->avatarUrl }}" alt="{{ $author->username }} - {{ config('app.name') }}">
@@ -17,11 +13,11 @@
     </div>
 
     <div class="gistlog">
-        <section class="my-8 px-4 sm:px-8 my-8">
+        <section class="px-4 my-8 sm:px-8">
             <h1 class="gistlog__title">{{ $author->name }}</h1>
 
-            <div class="font-light mx-auto table">
-                <span class="font-bold text-blue-dark no-underline">
+            <div class="table mx-auto font-light">
+                <span class="font-bold no-underline text-blue-dark">
                     {{ '@' . $author->username }}
                 </span>
             </div>
@@ -31,7 +27,7 @@
                 <div class="mb-12">
                     <a href="/{{ $author->username }}/{{ $gist->id }}" class="text-2xl font-medium">{{ $gist->title }}</a>
 
-                    <span class="block text-sm mb-3">
+                    <span class="block mb-3 text-sm">
                         Posted {{ $gist->createdAt->diffForHumans() }}
                     </span>
 
@@ -40,10 +36,10 @@
                 @endforeach
             </section>
             @if ($author->bio)
-                <section class="my-8 border-t border-gray-400 text-gray-500">
-                    <h2 class="text-xl my-2">Author Bio</h2>
-                    <div>{{ $author->bio }}</div>
-                </section>
+            <section class="mx-auto text-gray-500 border-t border-gray-200 mt-14" style="max-width: 612px">
+                <h2 class="mt-6 mb-1 text-xl text-gray-700">About {{ $author->name }}</h2>
+                <div>{{ $author->bio }}</div>
+            </section>
             @endif
         </section>
     </div>
